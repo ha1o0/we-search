14ea4bf2c4c1ea76e6f0aa2621374ad2e9e45b24
sidney <516202795@qq.com>
2023-04-18 00:12:42 +0800

    add vue router 4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter,createWebHistory, createWebHashHistory} from 'vue-router'
 import App from './App.vue'
+import { routes } from './router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = createApp(App)
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
