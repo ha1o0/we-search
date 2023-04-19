ca7d85526bc538858dddf8decaa81b36aedd668f
sidney <516202795@qq.com>
2023-04-19 21:38:52 +0800

    add lottie demo

--- src/pages/Anim.vue
+++ src/pages/Anim.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <canvas class="canvas" id="pag"></canvas>
+    <div><canvas class="canvas" id="pag"></canvas>
     <button @click="play">play</button>
-    <button @click="pause">pause</button>
+    <button @click="pause">pause</button></div>
+    <div><div ref="astro" style="width: 200px; height: 200px;"></div>
+    <button @click="playLottie">play-lottie</button>
+    <button @click="pauseLottie">pause-lottie</button></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted } from "@vue/runtime-core"
+  import { ref, onMounted, onBeforeUnmount } from "@vue/runtime-core"
+  import lottie from 'lottie-web'
 
   defineProps({
     msg: String
   })
+
+  let pagView = null
+  let lottieView = null
+  const astro = ref(null)
 
   onMounted(() => {
     console.log('mounted')
@@ -28,11 +36,24 @@
       canvas.height = pagFile.height();
       // Create PAGView.
       pagView = await PAG.PAGView.init(pagFile, canvas);
+      console.log(pagView)
       // Play PAGView.
     };
+
+    lottieView = lottie.loadAnimation({
+      container: astro.value, // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'http://static.nimi.eu.org/assets/loader.json' // the path to the animation json
+    });
+    console.log(lottie, astro, lottieView)
   })
 
-  let pagView = null
+  onBeforeUnmount(() => {
+    lottieView.destroy()
+    pagView.destroy()
+  })
 
   const play = () => {
     pagView.play();
@@ -42,7 +63,12 @@
     pagView.pause();
   }
 
+  const playLottie = () => {
+    lottieView.play();
+  }
 
-
+  const pauseLottie = () => {
+    lottieView.pause();
+  }
 
 </script>
